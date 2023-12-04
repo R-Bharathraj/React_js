43a53e0023a8d9cda2b7274dd9f16bbf009f662e
R-Bharathraj <bharathraj0535@gmail.com>
2023-12-04 15:41:41 +0530

    S.K Engineers

--- src/Roofing_website/Demo.js
+++ src/Roofing_website/Demo.js
@@ -1,13 +1,12 @@
 import React from 'react'
+import "./style/style.css"
+// import "./style/mediaquery.css"
 import Top_bar from './components/Top_bar'
 import Slider from './components/Slider'
 import Our_service from './components/Our_service'
 import Choice from './components/Choice'
 import Footer from './components/Footer'
 import Products from './components/Products'
-import "./style/style.css"
-import "./style/mediaquery.css"
-
 function Demo() {
   return (
    <React.Fragment>
